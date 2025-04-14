485b1cd3fd14e3aa869f2b3123acdb340b292d3f
Hooni-i <rlagns0904@gmail.com>
2025-04-14 23:32:12 +0900

    refactor: jwt 토큰 id String 값으로 변경 로그인 로직 상세 분기처리

--- src/main/java/com/fourirbnb/auth/config/AuthSecurityConfig.java
+++ src/main/java/com/fourirbnb/auth/config/AuthSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.fourirbnb.auth.config;
 
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import java.nio.charset.StandardCharsets;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,32 +15,6 @@ public class AuthSecurityConfig {
   @Value("${jwt.secret}")
   private String secret;
 
-//  @Bean
-//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http
-//        // CSRF 보호 비활성화
-//        .csrf(AbstractHttpConfigurer::disable)
-//        .authorizeHttpRequests(authorize -> authorize // 요청에 대한 접근 권한을 설정합니다.
-//            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//            .requestMatchers(
-//                "/api/auth/signUp",
-//                "/h2-console/**"
-//
-//            ).permitAll()
-//            // 그 외의 모든 요청은 인증이 필요합니다.
-//            .anyRequest().authenticated()
-//        )
-//        .anonymous(Customizer.withDefaults())
-//        //STATELESS로 설정
-//        .sessionManagement(session -> session
-//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//        )
-//        .headers(headers -> headers
-//            .frameOptions(FrameOptionsConfig::sameOrigin)
-//        );
-//    return http.build();
-//  }
-
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
@@ -48,7 +22,7 @@ public class AuthSecurityConfig {
 
   @Bean
   public SecretKey jwtSecretKey() {
-    return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
   }
 
 }
